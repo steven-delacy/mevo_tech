42450ce5de553f198fdecac0169fcda8d6042c74
steven <steven.norhed@gmail.com>
2021-01-31 15:04:15 +1300

    have mapbox working, next to access mevo API

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,31 +1,31 @@
-import React, { useState } from "react";
-import ReactMapGl from "react-map-gl";
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom'
+import ReactMapGL, { Marker } from 'react-map-gl';
 
-export default function App(){
 
+export default function App() {
 
-  // Have state use hooks to declare viewports of vehicles
   const [viewport, setViewport] = useState({
-    latitude: -41.294105529785156,
-    longitude:  174.7752685546875,
-    zoom: 10,
+    latitude: -41.28664,
+    longitude: 174.77557,
     width: "100vw",
-    height: "100vh"
-    
-  })
-
-    return (
-      <>
-      <div>
-        <ReactMapGl 
-        {...viewport} 
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}>
-          car makers here
-        </ReactMapGl>
-      </div>
-      </>
-
-    );
-  }
+    height: "100vh",
+    zoom: 10
+  });
 
 
+  return (
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken='pk.eyJ1Ijoic3RldmVuLWRlbGFjeSIsImEiOiJja2trZ2VoOW8xajh2Mm9ud2RvNGx1eXg4In0.RNdzxrMk75w35M9Nxfyg6g'
+        onViewportChange={(viewport) => {
+          setViewport(viewport)
+        }}
+      >
+
+
+      </ReactMapGL>
+    </div>
+  );
+}
